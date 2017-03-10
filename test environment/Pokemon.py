ee1d7d55c8b79ad23ea251bbf2c089c35129c3cd
Thomas Spader <thomas.spader@gatech.edu>
2017-03-10 16:59:17 -0500

    First commit

---
+++ test environment/Pokemon.py
@@ -0,0 +1,48 @@
+import json
+
+class Pokemon():
+	def __init__(self, name, typing=None, stats=None, moves=None, isBurned=False, isParalyzed=False, isAsleep=False, isFrozen=False):
+		self.name = name
+		self.isFrozen = isFrozen
+		self.isAsleep = isAsleep
+		self.isParalyzed = isParalyzed
+		self.isBurned = isBurned
+		if stats is None:
+			self.stats = {}
+		if typing is None:
+			self.typing = []
+		if moves is None:
+			self.moves = []
+
+		with open("../data/pokemon.json", "r") as infile:
+			data = json.load(infile)
+			for stat in data[self.name]["stats"]:
+				self.stats[stat] = data[name]["stats"][stat]
+				if stat == "hp":
+					self.hp = data[name]["stats"][stat]
+			for _type in data[name]["type"]:
+				self.typing.append(_type)
+
+
+	def __str__(self):
+		# pokemonString = "name: " + self.name + "\nhp: " + str(self.hp) + "\nmoves:"
+		# for move in self.moves:
+		# 	pokemonString += " " + move
+		# pokemonString += "\n--------"
+		# return pokemonString
+		return self.name
+
+	def addMove(self, move):
+		self.moves.append(move)
+
+	def isFainted(self):
+		return self.hp <= 0
+
+if __name__ == "__main__":
+	jarvis = Pokemon("Tauros")
+	wayne = Pokemon("Exeggutor")
+	print(jarvis.stats["hp"])
+	print(jarvis.isFainted())
+	print(wayne.stats)
+	print(jarvis.typing)
+
